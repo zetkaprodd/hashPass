02065ed7160bba1ed2c92134fda4799518ce6ee0
Ebayart <elibayart05@gmail.com>
2024-12-14 15:32:58 +0100

    feat: add utils functions

---
+++ src/utils.py
@@ -0,0 +1,12 @@
+def printMenu():
+    print("1 - Changer le mot de passe maître.")
+    print("1 - Générer un mot de passe aléatoire.")
+    print("1 - Générer un mot de pass depuis un tag et un mot de passe maître.")
+
+
+def askTag():
+    return input("Entrez le tag: ")
+
+def askMasterPassword():
+    return input("Entrez le mot de passe maître: ")
+
